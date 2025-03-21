0a930c3a74af51c86ddb47d151775b9d20b5e8e9
Andreas Nilsson <codr7@protonmail.com>
2025-03-21 03:27:16 +0100

    Update benchmark.

--- fix/benchmark.c
+++ fix/benchmark.c
@@ -3,20 +3,20 @@
 
 void fix_benchmark() {
   struct hc_timer t;
-  const int n = 10000;
+  const int n = 100000;
 
   hc_timer_init(&t);
   double dv = 0;
   
   for (int i = 0; i < n; i++) {
-    dv += 0.01;
+    dv += 0.001;
   }
 
   hc_timer_print(&t, "double: "); 
 
   hc_timer_init(&t);
   hc_fix fv = hc_fix_new(HC_FIX_MAX_EXP, 0);
-  hc_fix fd = hc_fix_new(2, 1);
+  hc_fix fd = hc_fix_new(3, 1);
   
   for (int i = 0; i < n; i++) {
     fv = hc_fix_add(fv, fd);
